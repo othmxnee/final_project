7f5386ebfe9e165be67c73434b10fa7eaacef6c9
othmxnee <161128946+othmxnee@users.noreply.github.com>
2024-05-09 21:06:52 +0100

    videoCall

--- DRF_Auth/django_rest_auth/settings.py
+++ DRF_Auth/django_rest_auth/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'promo',
-    'csv_app'
+    'csv_app',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- DRF_Auth/django_rest_auth/urls.py
+++ DRF_Auth/django_rest_auth/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('promo/', include('promo.urls')), 
     path('api/v1/auth/',include('accounts.urls')),
     path('etudiant/', include('csv_app.urls')), 
+    path('', include('base.urls'))
 
 
 ]
